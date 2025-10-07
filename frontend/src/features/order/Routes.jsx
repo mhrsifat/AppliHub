575b55d0a83d4ceb507676ba5db97629fde8175a
HafizurRahmanSifat <muhammadhafizurrahmansifat@gmail.com>
2025-10-07 13:18:03 +0600

    7

--- frontend/src/features/order/Routes.jsx
+++ frontend/src/features/order/Routes.jsx
@@ -5,11 +5,15 @@ import { Route } from "react-router-dom";
 const OrderListPage = lazy(() => import("./pages/OrderListPage"));
 const OrderCreatePage = lazy(() => import("./pages/OrderCreatePage"));
 const OrderDetailsPage = lazy(() => import("./pages/OrderDetailsPage"));
+const NotFound = lazy(() => import("@/components/common/NotFound"));
 
-const orderRoutes = [
-  <Route key="order-list" path="orders" element={<OrderListPage />} />,
-  <Route key="order-create" path="orders/create" element={<OrderCreatePage />} />,
-  <Route key="order-details" path="orders/:id" element={<OrderDetailsPage />} />
-];
+const orderRoutes = (
+  <>
+    <Route path="orders" element={<OrderListPage />} />
+    <Route path="orders/create" element={<OrderCreatePage />} />
+    <Route path="orders/:id" element={<OrderDetailsPage />} />
+    <Route path="orders/*" element={<NotFound />} />
+  </>
+);
 
 export default orderRoutes;
