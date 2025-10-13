aa0cc72d5e290d6f6b8301ed190bcbdadd82567c
MhrSifat <mhrsifat@gmail.com>
2025-10-13 18:56:30 +0600

    me

--- api/routes/auth.php
+++ api/routes/auth.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FortifyController;
+use App\Http\Controllers\RefreshController;
 
 // Public routes
 Route::post('register', [FortifyController::class, 'register']);
 Route::post('login', [FortifyController::class, 'login']);
 Route::post('forgot-password', [FortifyController::class, 'forgotPassword']);
 Route::post('reset-password', [FortifyController::class, 'resetPassword']);
-Route::post('refresh', [FortifyController::class, 'refreshToken']); // refresh is public
+Route::post('refresh', [RefreshController::class, 'me']); // refresh is public
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
