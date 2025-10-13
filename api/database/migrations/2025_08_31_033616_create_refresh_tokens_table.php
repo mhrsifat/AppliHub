36d8157abf3c5384958222d4dc24e360d753c1fb
MhrSifat <mhrsifat@gmail.com>
2025-10-13 17:46:49 +0600

    drop

--- api/database/migrations/2025_08_31_033616_create_refresh_tokens_table.php
+++ api/database/migrations/2025_08_31_033616_create_refresh_tokens_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('refresh_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')
-                ->constrained()
-                ->onDelete('cascade');
             $table->string('device_name')->nullable();
             // migration
 $table->morphs('tokenable');
