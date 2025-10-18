fee7a715d163c6427e7e4e361ac0ddc2c6064d39
HafizurRahmanSifat <muhammadhafizurrahmansifat@gmail.com>
2025-10-18 13:33:45 +0600

    done2

--- frontend/src/features/chat/index.js
+++ frontend/src/features/chat/index.js
@@ -1,4 +1,4 @@
 // src/components/chat/index.js
 export { default as ChatWidget } from './ChatWidget';
-export { default as AdminChat } from '../features/chat/pages/AdminChat';
-export { default as AdminDashboard } from '../features/chat/pages/AdminDashboard';
+export { default as AdminChat } from '@/features/chat/pages/AdminChat';
+export { default as AdminDashboard } from '@/features/chat/pages/AdminDashboard';
